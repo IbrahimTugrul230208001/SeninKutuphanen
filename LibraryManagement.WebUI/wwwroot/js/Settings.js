e6d052bba04ef088b782758a5a4567b635e93ee8
İbrahim Tuğrul <230208001@ostimteknik.edu.tr>
2024-06-27 15:16:22 +0300

    .

--- LibraryManagement.WebUI/wwwroot/js/Settings.js
+++ LibraryManagement.WebUI/wwwroot/js/Settings.js
@@ -56,3 +56,62 @@ $(document).ready(function () {
         });
     });
 });
+
+$(document).ready(function () {
+    $("#removeProfilePicButton").click(function (event) {
+        event.preventDefault();
+
+        var userName = $("#userName").val();
+        $.ajax({
+            type: "PUT",
+            url: "/Settings/RemoveProfilePicture",
+            data: JSON.stringify(userName),
+            dataType: "json",
+            contentType: "application/json",
+
+            success: function (response) {
+                if (response.success) {
+                    alert("Profil fotoğrafı kaldırıldı.");
+                }
+                else {
+                    alert("Kaldırma esnasında bir hata oluştu.");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log("Error Details: ", xhr, status, error);
+                alert("Sunucu ile irtibat kurulurken bir hata oluştu.");
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#changePassword").click(function (event) {
+        event.preventDefault();
+        var user = {
+            Password: $("#TbxCurrentPassword").val(),
+            NewPassword: $("#TbxNewPassword").val(),
+            NewPasswordAgain: $("#TbxNewPasswordAgain").val()
+        };
+        $.ajax({
+            type: "PUT",
+            url: "/Settings/SetNewPassword",
+            data: JSON.stringify(user),
+            dataType: "json",
+            contentType: "application/json",
+
+            success: function (response) {
+                if (response.success) {
+                    alert("Şifre değiştirildi.");
+                }
+                else {
+                    alert("Değiştirme esnasında bir hata oluştu.");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.log("Error Details: ", xhr, status, error);
+                alert("Sunucu ile irtibat kurulurken bir hata oluştu.");
+            }
+        });
+    });
+});
