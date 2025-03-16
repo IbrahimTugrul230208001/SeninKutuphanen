437263c27f4a22a1d9fe2ae340d7faf470971745
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-03-16 16:16:39 +0300

    .

---
+++ LibraryManagement.WebUI/Controllers/ChatBotController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
+
+namespace learningASP.NET_CORE.Controllers
+{
+    public class ChatBotController : Controller
+    {
+        public IActionResult ChatBot()
+        {
+            return View();
+        }
+    }
+}
