1aee20c125eeafe5eae6327da5ca059390017f7b
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-04-03 12:56:32 +0300

    .

--- LibraryManagement.WebUI/Hubs/AIHub.cs
+++ LibraryManagement.WebUI/Hubs/AIHub.cs
@@ -4,9 +4,6 @@ namespace learningASP.NET_CORE.Hubs
 {
     public class AIHub:Hub
     {
-       public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
+
     }
 }
