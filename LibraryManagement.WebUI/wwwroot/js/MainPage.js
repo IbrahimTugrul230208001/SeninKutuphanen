705da5a84baa56415e624a6c1ca2397b6cb51ec8
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-06-25 13:51:05 +0300

    .

--- LibraryManagement.WebUI/wwwroot/js/MainPage.js
+++ LibraryManagement.WebUI/wwwroot/js/MainPage.js
@@ -42,3 +42,33 @@ $(document).ready(function () {
         });
     }
 });
+
+$(document).ready(function () {
+    $('.add-btn').on('click', function () {
+        var $btn = $(this);
+        if ($btn.data('state') === 'plus') {
+            // Animate checkmark drawing
+            $btn.removeClass('bg-blue-600 hover:bg-blue-700').addClass('bg-green-600 hover:bg-green-700');
+            $btn.find('.plus-icon').addClass('hidden');
+            var $checkIcon = $btn.find('.check-icon');
+            $checkIcon.removeClass('hidden');
+            var $svg = $checkIcon.find('svg');
+            var $path = $svg.find('path');
+            // Prepare for animation
+            var length = $path[0].getTotalLength();
+            $path.css({
+                'stroke-dasharray': length,
+                'stroke-dashoffset': -length, // Change offset to negative for left-to-right animation
+                'transition': 'none'
+            });
+            // Force reflow for transition to take effect
+            $path[0].getBoundingClientRect();
+            // Animate
+            $path.css({
+                'transition': 'stroke-dashoffset 0.5s ease',
+                'stroke-dashoffset': 0
+            });
+            $btn.data('state', 'check');
+        }
+    });
+});
